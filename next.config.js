e5223cd42bbaef91c5e35aebbff2634ff6a93594
joanjeremiah <81401721+joanjeremiah@users.noreply.github.com>
2021-06-18 23:53:50 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    env: {
+        MONGODB_URI: "mongodb+srv://joan1234:joan1234@cluster1.k51tm.mongodb.net/ecommerce2?retryWrites=true&w=majority",
+        NEXTAUTH_URL: "https://ecommerce6.waykey.vercel.app",
+        GOOGLE_CLIENT_ID : "502926959557-iuob3mb9lttjh35d441tg2rc80g988tc.apps.googleusercontent.com",
+        GOOGLE_CLIENT_SECRET : "UoHUrV37ADI9LTo-blGq5HBt"
+    }   
+}
