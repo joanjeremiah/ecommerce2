0d2148d6753cd6e209fe92a605a1bc4f4bc5a56b
joanjeremiah <81401721+joanjeremiah@users.noreply.github.com>
2021-06-18 18:21:38 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    env: {
-        MONGODB_URI: "mongodb+srv://joan1234:joan1234@cluster1.k51tm.mongodb.net/ecommerce2?retryWrites=true&w=majority",
-        NEXTAUTH_URL: "https://ecommerce.waykey.vercel.app",
-        GOOGLE_CLIENT_ID : "368583127834-lkqlpo7snugmcrj9eijqv7ee0iattj7v.apps.googleusercontent.com",
-        GOOGLE_CLIENT_SECRET : "4fVjagDUodj3OptU2W-28HOc"
-    }   
-}
